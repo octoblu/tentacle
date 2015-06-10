347f07662861859d52b1b7ec40f4b46eefb0fb8d
Square Root of Saturn <sqrt@octoblu.com>
2015-06-09 18:36:11 -0700

    did something with pins

--- tentacle.cpp
+++ tentacle.cpp
@@ -1,13 +1,4 @@
 #include "tentacle.h"
-Tentacle::Tentacle() {
-  numPins = getNumberOfPins();
-  config = new Pin[numPins];
-
-  for(int i = 0; i < numPins; i++) {
-    config[i] = Pin(i);
-  }
-
-}
 
 void Tentacle::configurePins(std::vector<Pin> pins) {
   std::vector<Pin>::iterator pin = pins.begin();
--- tentacle.h
+++ tentacle.h
@@ -6,17 +6,16 @@
 
 class Tentacle {
   public:
-    Tentacle();
     void configurePins(std::vector<Pin> pins);
     void configurePin(Pin pin);
-    virtual int getNumberOfPins();
-    virtual void setMode(Pin pin);
-    virtual void digitalWrite(int pin, int value);
-    virtual void analogWrite(int pin, int value);
-    virtual bool digitalRead(int pin);
-    virtual int analogRead(int pin);
+    virtual int getNumberOfPins() = 0;
+    virtual void setMode(Pin pin) = 0;
+    virtual void digitalWrite(int pin, int value) = 0;
+    virtual void analogWrite(int pin, int value) = 0;
+    virtual bool digitalRead(int pin) = 0;
+    virtual int analogRead(int pin) = 0;
     std::vector<Pin> getValue();
-  private:
+  protected:
     Pin *config;
     int numPins;
 };
